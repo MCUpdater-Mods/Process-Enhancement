fbef16b67a5ac3337996fa212f01ae79145d5701
Scott M. Barbour <smbarbour@gmail.com>
2026-03-30 00:51:31 -0500

    Initial planning for "grid"

--- src/main/java/com/mcupdater/procenhance/grid/Node.java
+++ src/main/java/com/mcupdater/procenhance/grid/Node.java
@@ -1,7 +1,19 @@
 package com.mcupdater.procenhance.grid;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-public record Node(UUID id, UUID parentGrid, Set<Node> neighbors){
+public class Node {
+
+	private final UUID nodeId;
+	private UUID gridId;
+	private Set<Node> neighborSet;
+
+	public Node(){
+		this.nodeId = UUID.randomUUID();
+		this.neighborSet = new HashSet<>();
+	}
+
+
 }
